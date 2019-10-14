947e1c69a15d49076a51538287998da1bbd1b1cf
stickysidedown <matthew.elkins@gmail.com>
2019-10-14 08:07:41 -0700

    Final array chapter push

---
+++ ruby_intro_book/Arrays/arrarys.rb
@@ -0,0 +1,3 @@
+array = [4,7,6,5,4,2]
+
+array.each_index {|i| puts "This is the index #{i}"}
---
+++ ruby_intro_book/Arrays/exercise_6_arrays.rb
@@ -0,0 +1,9 @@
+names = ['bob', 'joe', 'susan', 'margaret']
+names['margaret'] = 'jody'
+
+#Index access requires index number
+
+#TypeError: no implicit conversion of String into Integer
+ # from (irb):2:in `[]='
+  #from (irb):2
+  #from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
