0c59a3ded9758b54a9596ed4c7b1edfccca9c2b3
stickysidedown <matthew.elkins@gmail.com>
2019-10-14 07:10:02 -0700

    Exercise one of array chapter

---
+++ ruby_intro_book/Arrays/exercise_1_arrays.rb
@@ -0,0 +1,15 @@
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+if arr.include?(3)
+  puts "#{number} is in the array"
+end
+
+
+arr.each do |num|
+if num == number
+  puts "#{num} is in the array"
+  end
+end
+
+ 
